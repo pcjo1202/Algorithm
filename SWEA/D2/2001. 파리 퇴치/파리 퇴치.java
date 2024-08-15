fae23f38948053654d4359181f4c033531d94683
Chang jo Park <ckdwh1202@naver.com>
2024-08-15 18:42:19 +0900

    [D2] Title: 파리 퇴치, Time: 113 ms, Memory: 20,564 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -4,52 +4,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+    static int N, M, result;
     static int[][] board;
-    static boolean[][] visited;
-    static final int[] di = {-1, 1, 0, 0};
-    static final int[] dj = {0, 0, -1, 1};
-    static int N, M, max = 0;
-
-    static void dfs(int i, int j){
-        // 기저 조건
-        if(i == N -2 && j == M -2){
-            return;
-        }
-
-        // 해야할 일 : M * M 사이에 있는 사각형들의 합을 구해야함
-        visited[i][j] = true;
-        int subSum = 0;
-        for(int n = 0; n < M; n++){
-            for(int m = 0; m < M; m++){
-                subSum += board[i+n][j+m];
-            }
-        }
-
-        max = Math.max(max, subSum);
-
-        // 탐색
-        for (int d = 0; d < 4; d++) {
-            int ni = i + di[d];
-            int nj = j + dj[d];
-            if( 0 > ni || ni + M -1 > N -1  || 0 > nj || nj + M -1 > N -1 || visited[ni][nj]) continue;
-            dfs(ni, nj);
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
         StringBuilder sb = new StringBuilder();
+
         int T = Integer.parseInt(br.readLine());
 
         for (int test_case = 1; test_case <= T; test_case++) {
-            max = 0;
             st = new StringTokenizer(br.readLine());
+
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
+            result = 0;
 
             board = new int[N][N];
-            visited = new boolean[N][N];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -58,9 +30,19 @@ public class Solution {
                 }
             }
 
-            dfs(0, 0);
+            for (int i = 0; i <= N - M; i++) { //
+                for (int j = 0; j <= N - M; j++) {
+                    int temp = 0;
+                    for (int k = 0; k < M; k++) {
+                        for (int l = 0; l < M; l++) {
+                            temp += board[i + k][j + l];
+                        }
+                    }
+                    result = Math.max(temp, result);
+                }
+            }
 
-            sb.append("#").append(test_case).append(" ").append(max).append('\n');
+            sb.append("#").append(test_case).append(" ").append(result).append("\n");
         }
         System.out.println(sb);
     }
