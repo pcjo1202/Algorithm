163bf709a640dba248472e46b0040d0e247fb551
Chang jo Park <ckdwh1202@naver.com>
2024-08-28 16:19:44 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 224 ms, Memory: 25,612 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/README.md
@@ -0,0 +1,15 @@
+# [D4] 창용 마을 무리의 개수 - 7465 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWngfZVa9XwDFAQU) 
+
+### 성능 요약
+
+메모리: 25,612 KB, 시간: 224 ms, 코드길이: 2,227 Bytes
+
+### 제출 일자
+
+2024-08-28 16:19
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static int N, M;
+    static int[][] pair;
+    static int[] parents;
+    static int result;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken()); // 마을에 사는 사람 수
+            M = Integer.parseInt(st.nextToken()); // 관계 수
+            result = 0;
+
+            pair = new int[M][2];
+            parents = new int[N + 1]; // 1 ~ N+1번까지
+
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                pair[i][0] = Integer.parseInt(st.nextToken());
+                pair[i][1] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int i = 1; i < N + 1; i++) {
+                parents[i] = -1;
+            }
+
+            // 집합 관계 설정
+            for (int i = 0; i < M; i++) {
+                int a = pair[i][0];
+                int b = pair[i][1];
+
+                union(a, b);
+            }
+
+            // 루트 노드의 개수 파악 (음수)
+
+            for (int i = 1; i < N + 1; i++) {
+                if (parents[i] < 0) result++;
+            }
+            sb.append(String.format("#%d %d\n", test_case, result));
+        }
+        System.out.println(sb);
+    }
+
+    static int fine(int a) {
+        if (parents[a] < 0) return a; // 자기 자신이라면,
+        return parents[a] = fine(parents[a]);
+    }
+
+    static void union(int a, int b) {
+        int aRoot = fine(a);
+        int bRoot = fine(b);
+
+        if (aRoot == bRoot) return;
+
+        if (parents[aRoot] < parents[bRoot]) { // 집합의 크기가 더 크다면,
+            parents[aRoot] += parents[bRoot];
+            parents[bRoot] = aRoot;
+        } else {
+            parents[bRoot] += parents[aRoot];
+            parents[aRoot] = bRoot;
+        }
+    }
+}
