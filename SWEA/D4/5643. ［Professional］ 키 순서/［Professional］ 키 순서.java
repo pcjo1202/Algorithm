40f17dac8121f7f7d8c8d3f6e508e7c2e1915172
Chang jo Park <ckdwh1202@naver.com>
2024-09-06 10:15:41 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,696 ms, Memory: 92,168 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,13 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int N, M, result;
-    static int INF = 999999; // 적절한 큰 값으로 설정
-    static int[][] graph;
+    static int N, M, result, count;
+    static int[][] graph, rgraph;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = null;
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
@@ -19,47 +18,119 @@ public class Solution {
             M = Integer.parseInt(br.readLine());
 
             graph = new int[N + 1][N + 1];
-
-            for (int i = 1; i <= N; i++) {
-                Arrays.fill(graph[i], INF);
-                graph[i][i] = 0;
-            }
+            rgraph = new int[N + 1][N + 1];
 
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
 
-                graph[a][b] = 1; // a < b 관계를 나타냄
+                graph[a][b] = 1;
+                rgraph[b][a] = 1;
             }
 
             result = 0;
+            // 자신의 키가 몇 번째인지 알 수 있는 학생은 어떻게 구분??
+            // N명 중 나보다 작은 친구 수 + 큰 친구 수 == N - 1명이면 내가 몇번째인지 정확히 알 수 있을라나?
 
-            // 플로이드-워셜 알고리즘
-            for (int k = 1; k <= N; k++) {
-                for (int i = 1; i <= N; i++) {
-                    for (int j = 1; j <= N; j++) {
-                        if (graph[i][j] > graph[i][k] + graph[k][j]) {
-                            graph[i][j] = graph[i][k] + graph[k][j];
-                        }
-                    }
-                }
-            }
-
-            // 키 순서를 알 수 있는 학생 확인
-            for (int i = 1; i <= N; i++) {
-                boolean flag = true;
-                for (int j = 1; j <= N; j++) {
-                    if (graph[i][j] == INF && graph[j][i] == INF) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) result++;
+            for (int i = 1; i < N + 1; i++) { // 모든 친구를 기준으로 확인
+                count = 0;
+                findDFS(i, graph, new boolean[N + 1]);
+                findDFS(i, rgraph, new boolean[N + 1]);
+//                findInMeDFS(i, new boolean[N + 1]);
+//                findOutMeDFS(i, new boolean[N + 1]);
+                if (count == N - 1) result++;
+//                // 1. 나를 가리키고 있는 노드의 수
+//                int tempA = findInMe(i);
+//
+//                // 2. 내가 가리키고 있는 노드의 수
+//                int tempB = findOutMe(i);
+//
+//                // 3. 1번 2번의 합 확인
+//                if (tempA + tempB == N - 1) {
+//                    result++;
+//                }
             }
 
             sb.append(String.format("#%d %d\n", tc, result));
         }
         System.out.println(sb);
+    }
+
+    static void findDFS(int node, int[][] matrix, boolean[] visited) {
+        visited[node] = true;
+        for (int i = 1; i < N + 1; i++) {
+            if (!visited[i] && matrix[node][i] != 0) {
+                findDFS(i, matrix, visited);
+                count++;
+            }
+        }
+    }
+
+    static void findInMeDFS(int node, boolean[] visited) {
+        visited[node] = true;
+        for (int i = 1; i < N + 1; i++) {
+            if (!visited[i] && graph[i][node] != 0) {
+                findInMeDFS(i, visited);
+                count++;
+            }
+        }
+    }
+
+    static void findOutMeDFS(int node, boolean[] visited) {
+        visited[node] = true;
+        for (int i = 1; i < N + 1; i++) {
+            if (!visited[i] && graph[node][i] != 0) {
+                findOutMeDFS(i, visited);
+                count++;
+            }
+        }
+    }
+
+    static int findInMe(int node) {
+        boolean[] visited = new boolean[N + 1];
+        int cnt = 0;
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int i = 1; i < N + 1; i++) {
+                if (visited[i]) continue;
+                if (graph[i][cur] != 0) {
+                    queue.offer(i);
+                    visited[i] = true;
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+
+    static int findOutMe(int node) {
+        boolean[] visited = new boolean[N + 1];
+        int cnt = 0;
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int i = 1; i < N + 1; i++) {
+                if (visited[i]) continue;
+                if (graph[cur][i] != 0) {
+                    queue.offer(i);
+                    visited[i] = true;
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
     }
 }
