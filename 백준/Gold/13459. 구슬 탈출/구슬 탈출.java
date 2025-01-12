f856eb295ba160cf0f5d646a02978b386310a60f
Changjo <ckdwh1202@naver.com>
2025-01-12 19:09:45 +0900

    [Gold I] Title: 구슬 탈출, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
@@ -10,13 +10,11 @@ public class Main {
     static Node red, blue, hole;
 
     static class Node {
-        int redI, redJ, blueI, blueJ, count;
+        int i, j, count;
 
-        public Node(int redI, int redJ, int blueI, int blueJ, int count) {
-            this.redI = redI;
-            this.redJ = redJ;
-            this.blueI = blueI;
-            this.blueJ = blueJ;
+        public Node(int i, int j, int count) {
+            this.i = i;
+            this.j = j;
             this.count = count;
         }
     }
@@ -35,11 +33,11 @@ public class Main {
             for (int j = 0; j < M; j++) {
                 board[i][j] = str.charAt(j);
                 if (board[i][j] == 'R') {
-                    red = new Node(i, j, 0, 0, 0);
+                    red = new Node(i, j, 0);
                 } else if (board[i][j] == 'B') {
-                    blue = new Node(0, 0, i, j, 0);
+                    blue = new Node(i, j, 0);
                 } else if (board[i][j] == 'O') {
-                    hole = new Node(i, j, 0, 0, 0);
+                    hole = new Node(i, j, 0);
                 }
             }
         }
@@ -49,26 +47,28 @@ public class Main {
 
     static boolean bfs() {
         Queue<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(red.redI, red.redJ, blue.blueI, blue.blueJ, 0));
-        visited[red.redI][red.redJ][blue.blueI][blue.blueJ] = true;
+        queue.offer(new Node(red.i, red.j, 0));
+        queue.offer(new Node(blue.i, blue.j, 0));
+        visited[red.i][red.j][blue.i][blue.j] = true;
 
         while (!queue.isEmpty()) {
-            Node cur = queue.poll();
+            Node curRed = queue.poll();
+            Node curBlue = queue.poll();
 
-            if (cur.count > 10) break; // 최대 10번까지 이동 가능
-            if (check(cur)) return true; // 성공 조건 만족
+            if (curRed.count > 10 || curBlue.count > 10) break; // 최대 10번까지 이동 가능
+            if (check(curRed, curBlue)) return true; // 성공 조건 만족
 
             for (int d = 0; d < 4; d++) {
-                int[] nextRed = roll(cur.redI, cur.redJ, d);
-                int[] nextBlue = roll(cur.blueI, cur.blueJ, d);
+                int[] nextRed = roll(curRed.i, curRed.j, d);
+                int[] nextBlue = roll(curBlue.i, curBlue.j, d);
 
                 // 파란 구슬이 구멍에 빠지면 무시
                 if (board[nextBlue[0]][nextBlue[1]] == 'O') continue;
 
                 // 두 구슬이 같은 칸에 있을 경우, 더 많이 이동한 구슬을 뒤로 이동
                 if (nextRed[0] == nextBlue[0] && nextRed[1] == nextBlue[1]) {
-                    if (Math.abs(cur.redI - nextRed[0]) + Math.abs(cur.redJ - nextRed[1]) >
-                            Math.abs(cur.blueI - nextBlue[0]) + Math.abs(cur.blueJ - nextBlue[1])) {
+                    if (Math.abs(curRed.i - nextRed[0]) + Math.abs(curRed.j - nextRed[1]) >
+                            Math.abs(curBlue.i - nextBlue[0]) + Math.abs(curBlue.j - nextBlue[1])) {
                         nextRed[0] -= di[d];
                         nextRed[1] -= dj[d];
                     } else {
@@ -79,7 +79,8 @@ public class Main {
 
                 if (visited[nextRed[0]][nextRed[1]][nextBlue[0]][nextBlue[1]]) continue;
 
-                queue.offer(new Node(nextRed[0], nextRed[1], nextBlue[0], nextBlue[1], cur.count + 1));
+                queue.offer(new Node(nextRed[0], nextRed[1], curRed.count + 1));
+                queue.offer(new Node(nextBlue[0], nextBlue[1], curBlue.count + 1));
                 visited[nextRed[0]][nextRed[1]][nextBlue[0]][nextBlue[1]] = true;
             }
         }
@@ -99,11 +100,8 @@ public class Main {
         return new int[]{i, j};
     }
 
-    static boolean check(Node cur) {
+    static boolean check(Node curRed, Node curBlue) {
         // 빨간 구슬만 구멍에 들어갔을 때
-        if (board[cur.redI][cur.redJ] == 'O' && board[cur.blueI][cur.blueJ] != 'O') {
-            return true;
-        }
-        return false;
+        return board[curRed.i][curRed.j] == 'O' && board[curBlue.i][curBlue.j] != 'O';
     }
 }
