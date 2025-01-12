13f693fccedc9ddb219e825ad7f94b3d95442cab
Changjo <ckdwh1202@naver.com>
2025-01-12 20:37:27 +0900

    [level 2] Title: 디펜스 게임, Time: 143.86 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 최소힙
+        
+        for(int i = 0; i < enemy.length; i++){
+            pq.offer(enemy[i]);
+            
+            if(pq.size() <= k){
+                answer++;
+                continue;
+            }
+            n -= pq.poll();
+            if(n < 0) break;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
