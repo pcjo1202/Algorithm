85792730b6b923d3636fb2b234ef2758732d42a9
Changjo <ckdwh1202@naver.com>
2025-04-27 22:22:11 +0900

    [level 3] Title: 야근 지수, Time: 138.24 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -4,26 +4,15 @@ class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue((o1, o2) -> {
+            return o2 - o1;
+        });
         
         for(int res : works){
             pq.offer(res);
         }
         
-        while (n > 0) {
-            int cur = pq.poll();
-            
-            if(cur == 0) break;
-            
-            cur -= 1;
-            pq.offer(cur);
-            n--;
-        }
-        
-        while(!pq.isEmpty()){
-            int temp = pq.poll();
-            answer += temp * temp;
-        }
+        System.out.println(pq);
         
         return answer;
     }
