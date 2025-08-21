3c12a011a146af9253341007597f210b508a7d39
Changjo <ckdwh1202@naver.com>
2025-08-21 17:45:58 +0900

    [level 2] Title: 오픈채팅방, Time: 159.71 ms, Memory: 97.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,46 @@
+function solution(record) {
+    var answer = [];
+    
+    // userId - nickname 관리 자료구조
+    // {userId : nickname}
+    const status = new Map(); // userId : nickname
+    
+    record.forEach(each =>{
+        const [command, userId, nickname] = each.split(" ")
+        
+        switch(command){
+            case "Enter":
+                status.set(userId, nickname)
+                answer.push({userId, command})
+                break;
+            case "Leave":
+                answer.push({userId, command})
+                break;
+            case "Change":
+                status.set(userId, nickname)
+                break;
+        }
+    })
+    
+    answer = answer.map(({userId, command}) => {
+        const nickname = status.get(userId)
+        if(command === "Enter"){
+            return enterString(nickname)
+        }else if(command === "Leave"){
+            return leaveString(nickname)
+        }
+    })
+    
+    return answer;
+}
+
+const changeNickname = (arr, userId, nickname) =>{
+    arr.forEach(e =>{
+        if(e.userId === userId){
+            e.nickname = nickname
+        }
+    })
+}
+
+const enterString = (name) => `${name}님이 들어왔습니다.`
+const leaveString = (name) => `${name}님이 나갔습니다.`
